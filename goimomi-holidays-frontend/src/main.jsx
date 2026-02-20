ceecca5620fb2880a2d0aad42937476fefd28f62
Kabish20 <kabishboby@gmail.com>
2026-02-20 23:38:05 +0530

    Fix protocol-relative URL bug in axios baseURL

--- goimomi-holidays-frontend/src/main.jsx
+++ goimomi-holidays-frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './index.css'
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.PROD ? '/' : 'http://127.0.0.1:8000'
+axios.defaults.baseURL = import.meta.env.PROD ? '' : 'http://127.0.0.1:8000'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
