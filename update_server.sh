667bf13762942ad95bb946ceefaa5e5c1fcf0a23
Kabish20 <kabishboby@gmail.com>
2026-05-21 12:32:58 +0530

    feat: add deployment scripts to automate packaging, file extraction, and server updates

---
+++ update_server.sh
@@ -0,0 +1,15 @@
+unzip -o update_package.zip -d /home/ubuntu/goimomi
+cd /home/ubuntu/goimomi/goimomi-holidays-backend
+
+# Update ALLOWED_HOSTS
+sed -i 's/^ALLOWED_HOSTS=.*/ALLOWED_HOSTS=3.27.184.234,localhost,127.0.0.1,goimomi.com,www.goimomi.com/' .env
+
+# Restart backend
+source venv/bin/activate
+python manage.py collectstatic --noinput
+sudo systemctl restart goimomi
+
+# Build frontend
+cd ../goimomi-holidays-frontend
+npm install
+npm run build
